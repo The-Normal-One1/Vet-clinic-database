8fdde29ebee6599a8601d1b6fccd5a0ce8d6e169
Ermi <manermidem@gmail.com>
2023-03-28 18:14:40 +0300

    Remove files

--- schema.sql
+++ schema.sql
@@ -1,9 +1,5 @@
 /* Database schema to keep the structure of entire database. */
 
--- CREATE TABLE animals (
---     name varchar(100)
--- );
-
 CREATE TABLE animals(
  id INT GENERATED ALWAYS AS IDENTITY,
 name VARCHAR(100) NOT NULL,

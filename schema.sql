d57f319543627a87a624d9597ec671baa47e4b89
Ermi <manermidem@gmail.com>
2023-03-28 16:55:28 +0300

    Edit the file

--- schema.sql
+++ schema.sql
@@ -5,10 +5,11 @@
 -- );
 
 CREATE TABLE animals(
-id INT PRIMARY KEY NOT NULL,
+ id INT GENERATED ALWAYS AS IDENTITY,
 name VARCHAR(100) NOT NULL,
 date_of_birth DATE NULL,
 escape_attempts INT NOT NULL,
 neutered BOOLEAN NOT NULL,
-weight_kg DECIMAL NOT NULL
+weight_kg DECIMAL NOT NULL,
+PRIMARY KEY(id)
 );

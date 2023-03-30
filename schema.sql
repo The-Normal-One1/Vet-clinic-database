50ac70fa4ed25c344443df3bafc233770950ba8d
Ermi <manermidem@gmail.com>
2023-03-30 13:06:26 +0300

    Create tables for species and owners

--- schema.sql
+++ schema.sql
@@ -12,3 +12,49 @@ PRIMARY KEY(id)
 
 ALTER TABLE animals
 ADD species TEXT;
+
+--  ADDING OWNERS AND SPECIES TABELS
+
+SELECT * FROM animals
+
+CREATE TABLE owners(
+id INT GENERATED ALWAYS AS IDENTITY,
+	full_name  VARCHAR(30),
+	age INT,
+	PRIMARY KEY(id)
+);
+
+SELECT * FROM owners
+
+CREATE TABLE species(
+id INT GENERATED ALWAYS AS IDENTITY,
+	name  VARCHAR(30),
+	PRIMARY KEY(id)
+);
+
+SELECT * FROM species
+
+ALTER TABLE animals
+DROP species;
+
+SELECT * FROM animals
+
+ALTER TABLE animals
+ADD species_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+SELECT * FROM animals
+
+ALTER TABLE animals
+ADD owner_id INT;
+
+SELECT * FROM animals
+
+ALTER TABLE animals
+ADD FOREIGN KEY (owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
